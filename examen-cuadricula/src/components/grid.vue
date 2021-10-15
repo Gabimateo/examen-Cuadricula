33a324e32e0c080e8e917a2156b51c3348e401ba
Gabimateo <gabimateoseg@gmail.com>
2021-10-15 10:58:13 +0200

    0.0.33

---
+++ examen-cuadricula/src/components/grid.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+ <div class="container">
+   <div class="items">
+     1
+   </div><div class="items">
+     1
+   </div><div class="items">
+     1
+   </div><div class="items">
+     1
+   </div><div class="items">
+     1
+   </div><div class="items">
+     1
+   </div><div class="items">
+     1
+   </div>
+ </div>
+</div>
+  
+</template>
+
+<script>
+export default {
+  name: 'Grid'
+}
+</script>
+
+<style lang="stylus">
+
+</style>
